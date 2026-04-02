b9203d0a3445151ec1112cc74b3a1d1b6b5a9255
manojacccenture-wq <manojacccenture@gmail.com>
2026-04-02 16:25:38 +0530

    fixed_cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,26 @@ import cors from "cors"
 import express from "express";
 import cookieParser from "cookie-parser";
 import routesIndex from "./routes/v1/index.js"
-import {errorMiddleware} from "./shared/middleware/errorHanlder/error.middleware.js"
+import { errorMiddleware } from "./shared/middleware/errorHanlder/error.middleware.js"
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mmsaas.vercel.app"
+];
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (Postman, mobile apps, etc.)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   })
 );
@@ -15,9 +29,9 @@ app.use(
 // Middleware
 
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-app.use("/v1",routesIndex)
+app.use("/v1", routesIndex)
 app.use(errorMiddleware);
 
 
